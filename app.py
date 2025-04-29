a2372ffb41c514b06d6393c7460496da6f724646
IanL73 <ianl73@nycstudents.net>
2025-04-29 09:24:32 -0400

    hee hee hoo hoo

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 # Route for the home page
 @app.route("/")
 def index():
-    # We ask the Pokémon API for the first 150 Pokémon.
+    # We ask the Pokémon API for every Pokemons. Ever. All of them.
     response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=1025")
-    data = response.json()
-    pokemon_list = data['results']
-    
+    pokemondata = response.json()
+    pokemon_list = pokemondata['results']
+    response = requests.get("https://pokeapi.co/api/v2/move?limit=937")
+    movedata = response.json()
+    move_list = movedata['results']
+
     # We create a list to store details for each Pokémon.
     pokemons = []
     
@@ -37,18 +40,18 @@ def index():
 def pokemon_detail(id):
     # We get detailed info for a specific Pokémon using its id.
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}")
-    data = response.json()
+    pokemondata = response.json()
     
     # We extract extra details like types, height, weight, and stats.
-    types = [t['type']['name'] for t in data['types']]
-    height = data.get('height')
-    weight = data.get('weight')
-    name = data.get('name').capitalize()
+    types = [t['type']['name'] for t in pokemondata['types']]
+    height = pokemondata.get('height')
+    weight = pokemondata.get('weight')
+    name = pokemondata.get('name').capitalize()
     image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
     
     # Get the Pokémon’s base stats (like hp, attack, defense, etc.)
-    stat_names = [stat['stat']['name'] for stat in data['stats']]
-    stat_values = [stat['base_stat'] for stat in data['stats']]
+    stat_names = [stat['stat']['name'] for stat in pokemondata['stats']]
+    stat_values = [stat['base_stat'] for stat in pokemondata['stats']]
     
     # We tell Flask to show the 'pokemon.html' page with all these details.
     return render_template("pokemon.html", pokemon={
