0f96f3a7e514364e5a16c3508f01088e50289d6d
IanL73 <ianl73@nycstudents.net>
2025-05-19 09:26:27 -0400

    car :3

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ import requests
 
 app = Flask(__name__)
 
-# Route for the home page
+""" # Route for the home page
 @app.route("/")
-def index():
-    # We ask the Pokémon API for every Pokemons. Ever. All of them.
-    response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=1025")
-    pokemondata = response.json()
-    pokemon_list = pokemondata['results']
+def index(): """
+# We ask the Pokémon API for every Pokemons. Ever. All of them.
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=251")
+data = response.json
+for result in data['results']:
+    print(result['name'])
+
+"---------  _______________________      vroom vroom "
+"========  / -------____   ______  \ "
+"=======  |   /         | |      \  |        *beep* "
+"------- /   |__________| |_______|  \______"
+"====== |  ______  ----  |          ______  \ "
+"------ | /  ----\       | ------- /      \  |"
+"====== ||   []   |______|________|   []   |_|"
+"-------- \______//               \\______/"
