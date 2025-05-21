a72e13f4ef33ed9430e6995b1b04a800251b2103
IanL73 <ianl73@nycstudents.net>
2025-05-21 09:24:32 -0400

    WHY IS IT A STRING. WHY IS IT A STRING

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index(): """
 # We ask the Pokémon API for every Pokemons. Ever. All of them.
-response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=1025")
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=1&offset=1025")
 data = response.json()
 
 everypokemon = {} # We slot them all into a dictionary for later use.
@@ -17,12 +17,38 @@ for pokemon in data['results']:
     url = (pokemon['url'])
     url = url.split("/")
     pokeid = url[-2]
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokeid}")
+    pokemondata = response.json()
+    types = [t['type']['name'] for t in pokemondata['types']]
+    abilities = [a['ability']['name'] for a in pokemondata['abilities']]
     key = f"pokemon{pokeid}"
     everypokemon[key] = {
         'name': name,
-        'id': pokeid
+        'id': pokeid,
+        'types': types,
+        'abilities': abilities
     }
-print(everypokemon)
+while True:
+    question = random.randint(1, 1)
+    subject = random.randint(1, 1025)
+    if question == 1:
+        for pokemon in everypokemon:
+            if pokemon['id'] == subject:
+                answer = input(f"What is {pokemon['name']}'s primary type? ")
+                answer = answer.capitalize()
+                if answer == pokemon['types'][0].capitalize():
+                    print("Correct!")
+                else:
+                    print("Incorrect.")
+    elif question == 2:
+        for pokemon in everypokemon:
+            if pokemon['id'] == subject:
+                answer = input(f"What is {pokemon['name']}'s first ability? ")
+                answer = answer.capitalize()
+                if answer == pokemon['abilities'][0].capitalize().replace('-', ' '):
+                    print("correct")
+                else:
+                    print("Incorrect")
 
 
 "---------  _______________________      vroooooom "
